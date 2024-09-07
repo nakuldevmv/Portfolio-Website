51a4a12bacf7a45c9eebdd3b3a2e9276b8502540
Nakul Dev <nakuldev1561@gmail.com>
2024-09-07 21:32:20 +0530

    Update main_web.dart

--- portfolioweb/lib/nakuldev_portfolio/main_web.dart
+++ portfolioweb/lib/nakuldev_portfolio/main_web.dart
@@ -11,14 +11,49 @@ class _nakuldevState extends State<nakuldev> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "data",
-          style: TextStyle(color: Colors.amber),
+      // appBar: AppBar(
+      //   title: const Text("Nakul </Dev>"),
+      //   backgroundColor: Colors.grey,
+      //   shape: const BeveledRectangleBorder(
+      //       side: BorderSide(color: Colors.black, width: 2)),
+      // ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 70,
+              margin: const EdgeInsets.all(16),
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 162, 162, 162),
+                  borderRadius: BorderRadius.all(Radius.circular(15))),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Nakul </Dev>"),
+                  Text("data"),
+                  Text("data"),
+                  Text("data")
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              "WEBSITE UNDER DEVELOPMENT",
+              style:
+                  TextStyle(color: Colors.amber, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "~ Nakul </Dev>",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 218, 218, 218),
+                  fontWeight: FontWeight.w100),
+            )
+          ],
         ),
-      ),
-      body: const Column(
-        children: [Text("Nakul")],
       ),
     );
   }
