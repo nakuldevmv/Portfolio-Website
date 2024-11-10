ba23f25dac894a9a5e043ba6074cb45d4df6a451
Nakul Dev <nakuldev1561@gmail.com>
2024-11-10 23:51:10 +0530

    "Updated image files and Flutter bootstrap/service worker scripts"

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3839162594"
+    serviceWorkerVersion: "966843231"
   }
 });
